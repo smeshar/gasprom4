ed17a357485b18e1975579b57a77998f32823496
n0tle <facenotle@gmail.com>
2024-11-28 13:39:55 +0300

    Added files and added infinity registration menu

--- main.py
+++ main.py
@@ -25,47 +25,47 @@ def main():
 
     LOGO()
 
-    print(f""" Чтобы играть вам нужно {Fore.LIGHTRED_EX}зарегистрироваться{Fore.RESET}/{Fore.LIGHTGREEN_EX}войти{Fore.RESET} в аккаунт
-     {Fore.LIGHTRED_EX}Зарегистрироваться{Fore.RESET} 1
-     {Fore.LIGHTGREEN_EX}Войти в существующий аккаунт{Fore.RESET} 2""")
+    while True:
+        print(f""" Чтобы играть вам нужно {Fore.LIGHTRED_EX}зарегистрироваться{Fore.RESET}/{Fore.LIGHTGREEN_EX}войти{Fore.RESET} в аккаунт
+         {Fore.LIGHTRED_EX}Зарегистрироваться{Fore.RESET} 1
+         {Fore.LIGHTGREEN_EX}Войти в существующий аккаунт{Fore.RESET} 2""")
 
-    inp = int(input())
-    if inp == 1:
-        print('---')
-        print(
-            f' {Fore.LIGHTGREEN_EX}Введите ваш никнейм (может содержать буквы, цифры и специальные символы, максимальная длина 20, аккаунты с непристойными никнеймами будут удалены){Fore.RESET}')
-        nick = input()
-        print(f' {Fore.LIGHTBLUE_EX}Введите пароль (может содержать буквы, цифры и специальные символы, максимальная длина 20){Fore.RESET}')
-        psw = input()
-        if conn.register(nick, psw, balance, player_stocks):
+        inp = int(input())
+        if inp == 1:
+            print('---')
+            print(
+                f' {Fore.LIGHTGREEN_EX}Введите ваш никнейм (может содержать буквы, цифры и специальные символы, максимальная длина 20, аккаунты с непристойными никнеймами будут удалены){Fore.RESET}')
+            nick = input()
+            print(f' {Fore.LIGHTBLUE_EX}Введите пароль (может содержать буквы, цифры и специальные символы, максимальная длина 20){Fore.RESET}')
+            psw = input()
+            if conn.register(nick, psw, balance, player_stocks):
+                l = conn.login(nick, psw)
+                if len(l) == 0:
+                    inp = input()
+                id = l[0]
+                name = l[1]
+                print(f' {Fore.LIGHTYELLOW_EX}Успешная регистрация!{Fore.RESET}')
+                break
+            time.sleep(0.5)
+
+        elif inp == 2:
+            print('---')
+            print(
+                f' {Fore.LIGHTGREEN_EX}Введите ваш никнейм{Fore.RESET}')
+            nick = input()
+            print(f' {Fore.LIGHTBLUE_EX}Введите пароль{Fore.RESET}')
+            psw = input()
             l = conn.login(nick, psw)
             if len(l) == 0:
                 inp = input()
             id = l[0]
             name = l[1]
-            print(f' {Fore.LIGHTYELLOW_EX}Успешная регистрация!{Fore.RESET}')
-        time.sleep(0.5)
+            balance = l[3]
+            player_stocks = l[4]
+            print(f' {Fore.LIGHTYELLOW_EX}Успешная авторизация!{Fore.RESET}')
 
-    elif inp == 2:
-        print('---')
-        print(
-            f' {Fore.LIGHTGREEN_EX}Введите ваш никнейм{Fore.RESET}')
-        nick = input()
-        print(f' {Fore.LIGHTBLUE_EX}Введите пароль{Fore.RESET}')
-        psw = input()
-        l = conn.login(nick, psw)
-        if len(l) == 0:
-            inp = input()
-        id = l[0]
-        name = l[1]
-        balance = l[3]
-        player_stocks = l[4]
-        print(f' {Fore.LIGHTYELLOW_EX}Успешная авторизация!{Fore.RESET}')
-
-        time.sleep(1)
-
-    else:
-        inp = input()
+            time.sleep(1)
+            break
 
     while True:
 
