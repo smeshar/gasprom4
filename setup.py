f9c2927cc35737b9a53f38f14c907711614258f4
n0tle <facenotle@gmail.com>
2024-12-03 17:00:38 +0300

    ignored least sounds and changed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but they might need fine-tuning.
 build_exe_options = {
     "includes": ["colorama", "plotext", "key_generator"],
-    "include_files": ["buy.wav", "buy2.wav", "elec.wav", "load.wav", "save.wav", "sell.wav", "shop.wav"],
+    "include_files": ["buy2.wav", "sell.wav", "shop.wav"],
     "packages": ["pymysql"]
 }
 
